63c82af75a7aedff46ace666df3129b91383b107
dhchan <daniel.hoyin.chan@gmail.com>
2019-10-18 17:36:25 -0700

    all css changes
    Signed-off-by: dhchan <daniel.hoyin.chan@gmail.com>

--- index.js
+++ index.js
@@ -464,13 +464,14 @@ function loadModal() {
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
-    modal.style.display = "none";
+        modal.classList.add('hidden');
+
     }
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
         if (event.target == modal) {
-            modal.style.display = "none";
+            modal.classList.add('hidden');
         }
     }
 }
